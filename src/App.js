7b66a099089a27c14091d9fdc5d9712d97e3fef0
池亀 翔乃介 <shonosuke-ikegame@dip22-m10048.dip-net.local>
2023-02-06 17:26:28 +0900

    コンポーネント適用

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { studioTheme } from './ui-components'
 import { Amplify, API } from 'aws-amplify'
+import '@aws-amplify/ui-react/styles.css'
 import { useEffect, useState } from 'react'
+import { ThemeProvider } from '@aws-amplify/ui-react'
+import { NavBar, FAQItem } from './ui-components'
 import awsconfig from './aws-exports'
 Amplify.configure(awsconfig)
 
@@ -8,7 +12,6 @@ function App() {
   useEffect(() => {
     const execRq = async () => {
       const wikiObj = (await API.get('shonosukeapi', '/handson')).query.pages
-
       const wikiArr = Object.entries(wikiObj).map(([_, val]) => val)
       console.log(wikiArr)
       setWikis(wikiArr)
@@ -17,18 +20,16 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-           {' '}
+    <ThemeProvider theme={studioTheme}>
+      <NavBar></NavBar>
       {wikis.map((w) => {
+        //設定したコンポーネントプロパティのtitleとexplainプロパティにAPIから取得した値を入れてあげるだけ
         return (
-          <div key={w.pageid}>
-                        {w.title}
-            {w.extract}         {' '}
-          </div>
+          <FAQItem key={w.pageid} title={w.title} explain={w.extract}></FAQItem>
         )
       })}
-         {' '}
-    </div>
+    </ThemeProvider>
   )
 }
+
 export default App
